72d9da6f24d6574fc4a8641a502384f066d1bc43
Teffin <Teffin@mail.ru>
2024-11-05 22:16:00 +0300

    Initial Commit

---
+++ Sasflix/SasflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SasflixApp.swift
+//  Sasflix
+//
+//  Created by Evgeniy Kozyrev on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SasflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
